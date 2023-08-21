423c4772adb5dc2cd07f8db5246cb74db8911b78
Srikar Mk <srikarmks@gmail.com>
2023-08-21 11:07:48 +0530

    Added Projects

--- src/Components/Portfolio/Projects.jsx
+++ src/Components/Portfolio/Projects.jsx
@@ -1,12 +1,65 @@
+import deccls from "../../assets/decentraclasses.png"
+import supa from "../../assets/supa.png"
+import wigo from "../../assets/wigo.png"
+import tribecrafts from "../../assets/tribecrafts.png"
+import spherespin from "../../assets/spherespin.png"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGithub} from "@fortawesome/free-brands-svg-icons";
+import link from "../../assets/arrow.svg"
 const Projects = () => {
   return (
     <div id="projects" className="pl-5 mb-10">
       <h1 className="text-white text-5xl font-bold mb-10 flex justify-center">Projects Showcase</h1>
-      <div className="flex justify-evenly">
-        <div className="w-[30%] h-[40vh] bg-blue-300 shadow-[5px_5px_0px_0px_rgba(255,255,255,0.4)]">
-
+      <div className="flex justify-evenly mb-20">
+        <div className="w-[30%] bg-blue-300 shadow-[5px_5px_0px_0px_rgba(255,255,255)] p-5">
+          <img src={deccls} alt="decentraclasses" width={500} className="border-4 border-slate-500 mb-5" />
+          <h1 className="text-2xl font-semibold mb-5">Decentraclasses</h1>
+          <p className="mb-5">A Decentralised Ed-Tech Platform with Learn to Earn policy.</p>
+          <div className="flex space-x-10">
+          <a href="https://www.decentraclasses.com" target="_blank" rel="noreferrer"><button className="p-3 flex justify-between items-center space-x-2 border-2 shadow-[5px_5px_0px_0px_rgba(255,255,255,0.4)] hover:shadow-[3px_3px_0px_0px_rgba(255,255,255,0.4)]"><span>Visit Site</span><span className="mt-1"><img src={link} alt="" width={15} /></span></button></a>
+          <a href="https://github.com/DecentraSchool/decentraclasses" target="_blank" rel="noreferrer"><FontAwesomeIcon icon={faGithub} className="fa-3x hover:text-white" /></a>
+          </div>
         </div>
-        <div className="w-[30%] h-[40vh] bg-blue-300 shadow-[5px_5px_0px_0px_rgba(255,255,255,0.4)]"></div>
+        <div className="w-[30%] p-5 bg-blue-300 shadow-[5px_5px_0px_0px_rgba(255,255,255)]">
+          <img src={supa} alt="supasmoothies" width={500} className="border-4 border-slate-500 mb-5" />
+          <h1 className="text-2xl font-semibold mb-5">Supa Smoothies</h1>
+          <p className="mb-5">A Dynamic Smoothie menu made using React and Supabase.</p>
+          <div className="flex space-x-10">
+          <a href="https://supaa-smoothies.netlify.app" target="_blank" rel="noreferrer"><button className="p-3 flex justify-between items-center space-x-2 border-2 shadow-[5px_5px_0px_0px_rgba(255,255,255,0.4)] hover:shadow-[3px_3px_0px_0px_rgba(255,255,255,0.4)]"><span>Visit Site</span><span className="mt-1"><img src={link} alt="" width={15} /></span></button></a>
+          <a href="https://github.com/Srikarmk/Supa-Smoothies" target="_blank" rel="noreferrer"><FontAwesomeIcon icon={faGithub} className="fa-3x hover:text-white" /></a>
+          </div>
+          </div>
+      </div>
+      <div className="flex justify-evenly mb-20">
+        <div className="w-[30%] bg-blue-300 shadow-[5px_5px_0px_0px_rgba(255,255,255)] p-5">
+          <img src={tribecrafts} alt="tribecrafts" width={500} className="border-4 border-slate-500 mb-5" />
+          <h1 className="text-2xl font-semibold mb-5">TribeCrafts</h1>
+          <p className="mb-5">A Full Stack MERN ECommerce Application based on Tribal Products.</p>
+          <div className="flex space-x-10">
+          <a href="https://tribecrafts.netlify.app" target="_blank" rel="noreferrer"><button className="p-3 flex justify-between items-center space-x-2 border-2 shadow-[5px_5px_0px_0px_rgba(255,255,255,0.4)] hover:shadow-[3px_3px_0px_0px_rgba(255,255,255,0.4)]"><span>Visit Site</span><span className="mt-1"><img src={link} alt="" width={15} /></span></button></a>
+          <a href="https://github.com/Srikarmk/tribecrafts" target="_blank" rel="noreferrer"><FontAwesomeIcon icon={faGithub} className="fa-3x hover:text-white" /></a>
+          </div>
+        </div>
+        <div className="w-[30%] p-5 bg-blue-300 shadow-[5px_5px_0px_0px_rgba(255,255,255)]">
+          <img src={wigo} alt="decentraclasses" width={500} className="border-4 border-slate-500 mb-5" />
+          <h1 className="text-2xl font-semibold mb-5">WigoCash</h1>
+          <p className="mb-5">A Beta landing page for an upcoming cryptocurrency swapping application.</p>
+          <div className="flex space-x-10">
+          <a href="https://wigo-cash.vercel.app" target="_blank" rel="noreferrer"><button className="p-3 flex justify-between items-center space-x-2 border-2 shadow-[5px_5px_0px_0px_rgba(255,255,255,0.4)] hover:shadow-[3px_3px_0px_0px_rgba(255,255,255,0.4)]"><span>Visit Site</span><span className="mt-1"><img src={link} alt="" width={15} /></span></button></a>
+          <a href="https://github.com/Srikarmk/Wigo-Cash" target="_blank" rel="noreferrer"><FontAwesomeIcon icon={faGithub} className="fa-3x hover:text-white" /></a>
+          </div>
+          </div>
+      </div>
+      <div className="flex justify-evenly mb-20">
+        <div className="w-[30%] bg-blue-300 shadow-[5px_5px_0px_0px_rgba(255,255,255)] p-5">
+          <img src={spherespin} alt="spherespin" width={500} className="border-4 border-slate-500 mb-5" />
+          <h1 className="text-2xl font-semibold mb-5">Sphere Spin</h1>
+          <p className="mb-5">A Simple Three Js Orbital Sphere with color magic.</p>
+          <div className="flex space-x-10">
+          <a href="https://sphere-spin.netlify.app" target="_blank" rel="noreferrer"><button className="p-3 flex justify-between items-center space-x-2 border-2 shadow-[5px_5px_0px_0px_rgba(255,255,255,0.4)] hover:shadow-[3px_3px_0px_0px_rgba(255,255,255,0.4)]"><span>Visit Site</span><span className="mt-1"><img src={link} alt="" width={15} /></span></button></a>
+          <a href="https://github.com/Srikarmk/SphereSpin" target="_blank" rel="noreferrer"><FontAwesomeIcon icon={faGithub} className="fa-3x hover:text-white" /></a>
+          </div>
+        </div>
       </div>
     </div>
   )
